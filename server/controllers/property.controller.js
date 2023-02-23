f7266e8c90dba7bc3f55f0c4e27ed25c7c3cde38
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-02-23 16:53:46 -0500

    updated user controller

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -3,14 +3,15 @@ import User from '../mongodb/models/user.js';
 
 const getAllProperties = async (req, res) => {};
 const getPropertyDetail = async (req, res) => {};
-const createProperty = async (req, res) => {};
+
+const createProperty = async (req, res) => {
+  const { title, description, propertyType, location, price, photo, email } = req.body;
+
+  // Start a new session...
+  const session = await mongoose.startSession();
+};
+
 const updateProperty = async (req, res) => {};
 const deleteProperty = async (req, res) => {};
 
-export {
-  getAllProperties,
-  getPropertyDetail,
-  createProperty,
-  updateProperty,
-  deleteProperty
-}
+export { getAllProperties, getPropertyDetail, createProperty, updateProperty, deleteProperty };
